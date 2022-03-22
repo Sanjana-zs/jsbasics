d91287b4c2b39555d1cbd746bc9da9bbfb60f3be
Sanjana0056 <sanjana.s@zopsmart.com>
2022-03-22 15:29:41 +0530

    assignments for day1

---
+++ assignments_day1/prime.js
@@ -0,0 +1,18 @@
+// function definition
+function checkPrime(num){
+     let isPrime = true;
+     const cond = Math.sqrt(num);
+
+     for( let div=2; div<=cond; div++ ) {
+         if( num % div == 0 ) {
+             isPrime = false;
+             break;
+         }
+     }
+
+     isPrime ? console.log("Prime Number") : console.log("Not Prime Number");
+}
+
+// function calling
+const num = 19;
+checkPrime(num);
